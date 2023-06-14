79320a55d0313e966b4295e47630dc9d8aed0f49
Chet Jones <chetjones003@gmail.com>
2023-06-14 11:56:01 -0500

    update solutions

--- binary_search/src/lib.rs
+++ binary_search/src/lib.rs
@@ -1,4 +1,19 @@
-pub fn binary_search(arr: &[usize], target: &usize) -> Option<usize> {}
+pub fn binary_search(arr: &[i64], target: &i64) -> Option<usize> {
+    let mut low: usize = 0;
+    let mut high: usize = arr.len() - 1;
+    while low <= high {
+        let midpoint: usize = low + ((high - low) / 2);
+        let value: &i64 = &arr[midpoint];
+        if value == target {
+            return Some(midpoint);
+        } else if value > target {
+            high = midpoint - 1
+        } else if value < target {
+            low = midpoint + 1
+        }
+    }
+    None
+}
 
 #[cfg(test)]
 mod tests {
@@ -6,15 +21,18 @@ mod tests {
 
     #[test]
     fn basic_tests() {
-        assert_eq!(binary_search(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], &1), Some(1));
-        assert_eq!(binary_search(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], &2), Some(2));
-        assert_eq!(binary_search(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], &3), Some(3));
-        assert_eq!(binary_search(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], &4), Some(4));
-        assert_eq!(binary_search(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], &5), Some(5));
-        assert_eq!(binary_search(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], &6), Some(6));
-        assert_eq!(binary_search(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], &7), Some(7));
-        assert_eq!(binary_search(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], &8), Some(8));
-        assert_eq!(binary_search(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], &9), Some(9));
-        assert_eq!(binary_search(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], &0), Some(0));
+        assert_eq!(binary_search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], &1), Some(0));
+        assert_eq!(binary_search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], &2), Some(1));
+        assert_eq!(binary_search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], &3), Some(2));
+        assert_eq!(binary_search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], &4), Some(3));
+        assert_eq!(binary_search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], &5), Some(4));
+        assert_eq!(binary_search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], &6), Some(5));
+        assert_eq!(binary_search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], &7), Some(6));
+        assert_eq!(binary_search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], &8), Some(7));
+        assert_eq!(binary_search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], &9), Some(8));
+        assert_eq!(
+            binary_search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], &10),
+            Some(9)
+        );
     }
 }
--- linear_search/src/lib.rs
+++ linear_search/src/lib.rs
@@ -1,66 +1,62 @@
-// Simplest form to work with integers
-pub fn linear_search_ints(arr: &[i32], target: &i32) -> Option<usize> {}
-
-// Another version to search chars
-pub fn linear_search_chars(arr: &[char], target: &char) -> Option<usize> {}
-
-// Another version to search strings
-pub fn linear_search_strings(arr: &[String], target: &str) -> Option<usize> {}
-
 // Implementing a generic version
-pub fn linear_search<T: PartialEq>(arr: &[T], target: &T) -> Option<usize> {}
+pub fn linear_search<T: PartialEq>(arr: &[T], target: &T) -> Option<usize> {
+    for (index, item) in arr.iter().enumerate() {
+        if item == target {
+            return Some(index);
+        }
+    }
+    None
+}
 
 #[cfg(test)]
 mod test {
-    use super::linear_search_chars;
-    use super::linear_search_ints;
-    use super::linear_search_strings;
+    use super::linear_search;
 
     #[test]
     fn search_ints() {
-        assert_eq!(linear_search_ints(&[1, 2, 3, 4], &1), Some(0));
-        assert_eq!(linear_search_ints(&[1, 2, 3, 4], &2), Some(1));
-        assert_eq!(linear_search_ints(&[1, 2, 3, 4], &3), Some(2));
-        assert_eq!(linear_search_ints(&[1, 2, 3, 4], &4), Some(3));
-        assert_eq!(linear_search_ints(&[1, 2, 3, 4], &5), None);
+        assert_eq!(linear_search(&[1, 2, 3, 4], &1), Some(0));
+        assert_eq!(linear_search(&[1, 2, 3, 4], &2), Some(1));
+        assert_eq!(linear_search(&[1, 2, 3, 4], &3), Some(2));
+        assert_eq!(linear_search(&[1, 2, 3, 4], &4), Some(3));
+        assert_eq!(linear_search(&[1, 2, 3, 4], &5), None);
     }
 
     #[test]
     fn search_chars() {
-        assert_eq!(linear_search_chars(&['a', 'b', 'c', 'd'], &'a'), Some(0));
-        assert_eq!(linear_search_chars(&['a', 'b', 'c', 'd'], &'b'), Some(1));
-        assert_eq!(linear_search_chars(&['a', 'b', 'c', 'd'], &'c'), Some(2));
-        assert_eq!(linear_search_chars(&['a', 'b', 'c', 'd'], &'d'), Some(3));
-        assert_eq!(linear_search_chars(&['a', 'b', 'c', 'd'], &'e'), None);
+        assert_eq!(linear_search(&['a', 'b', 'c', 'd'], &'a'), Some(0));
+        assert_eq!(linear_search(&['a', 'b', 'c', 'd'], &'b'), Some(1));
+        assert_eq!(linear_search(&['a', 'b', 'c', 'd'], &'c'), Some(2));
+        assert_eq!(linear_search(&['a', 'b', 'c', 'd'], &'d'), Some(3));
+        assert_eq!(linear_search(&['a', 'b', 'c', 'd'], &'e'), None);
     }
 
     #[test]
     fn search_strings() {
         assert_eq!(
-            linear_search_strings(
+            linear_search(
                 &["Rust".to_string(), "is".to_string(), "Great".to_string()],
-                &"Rust"
+                &"Rust".to_string()
             ),
             Some(0)
         );
         assert_eq!(
-            linear_search_strings(
+            linear_search(
                 &["Rust".to_string(), "is".to_string(), "Great".to_string()],
-                &"is"
+                &"is".to_string()
             ),
             Some(1)
         );
         assert_eq!(
-            linear_search_strings(
+            linear_search(
                 &["Rust".to_string(), "is".to_string(), "Great".to_string()],
-                &"Great"
+                &"Great".to_string()
             ),
             Some(2)
         );
         assert_eq!(
-            linear_search_strings(
+            linear_search(
                 &["Rust".to_string(), "is".to_string(), "Great".to_string()],
-                &"Not"
+                &"Not".to_string()
             ),
             None
         );
