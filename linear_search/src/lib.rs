ec948ae4b6ef8085f1cd79732406131b89dbb9ac
Chet Jones <chetjones003@gmail.com>
2023-06-15 14:06:37 -0500

    finished

--- linear_search/src/lib.rs
+++ linear_search/src/lib.rs
@@ -1,8 +1,9 @@
 // Implementing a generic version
 pub fn linear_search<T: PartialEq>(arr: &[T], target: &T) -> Option<usize> {
     for (index, item) in arr.iter().enumerate() {
-        if item == target {
-            return Some(index);
+        match item == target {
+            true => return Some(index),
+            false => (),
         }
     }
     None
