3303221c40b62c51a0ebc4b7aa9899c55c77215e
Chet Jones <chetjones003@gmail.com>
2023-06-23 10:56:00 -0500

    refactor to return an Optional: List[int] | None

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python3
 
-from typing import List
+from typing import List, Optional
 
 
-def bubble_sort(arr: List[int]):
+def bubble_sort(arr: List[int]) -> Optional[List[int]]:
     swapped: bool = False
 
-    for i in range(len(arr) - 1):
-        for j in range(len(arr) - 1 - i):
-            if arr[j] > arr[j + 1]:
-                tmp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = tmp
-                swapped = True
+    if len(arr) == 0:
+        return None
+    else:
+        for i in range(len(arr) - 1):
+            for j in range(len(arr) - 1 - i):
+                if arr[j] > arr[j + 1]:
+                    tmp = arr[j]
+                    arr[j] = arr[j + 1]
+                    arr[j + 1] = tmp
+                    swapped = True
 
     if swapped:
         return arr
 
 
 def test_sorted_array():
-    arr = [1, 3, 4, 7, 2]
-    assert bubble_sort(arr) == [1, 2, 3, 4, 7]
+    assert bubble_sort([1, 3, 4, 7, 2]) == [1, 2, 3, 4, 7]
+    assert bubble_sort([]) == None
